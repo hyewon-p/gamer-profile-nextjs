94e122585bbbadbe98bc3f7c78a72871bd7a92b8
hyewon <hyewon.parkhw@gmail.com>
2023-05-15 16:57:30 +0900

    trait table fix

--- components/Trait.tsx
+++ components/Trait.tsx
@@ -61,86 +61,25 @@ const Trait = () => {
             </button>
           )}
         </div>
-        {data.length > 0 && (
-          <table className="border border-blue-400 rounded-t-md border-separate border-spacing-0 border-b-0 w-full">
-            <tbody>
-              {data.map((t, i: number) => (
-                <tr key={i} className="relative">
-                  <td
-                    width="25%"
-                    className="font-medium border-blue-400 border-r border-b leading-3"
-                  >
-                    {isOwner && (
-                      <div className="absolute left-[-1.5rem] h-full justify-center flex flex-col gap-2">
-                        <button
-                          type="button"
-                          onClick={() => {
-                            setData((prev) =>
-                              prev.filter((g, index) => index != i)
-                            );
-                            setTrash((prev) => [...prev, t.id]);
-                          }}
-                          className="group w-4 h-4 p-[0.1rem]"
-                        >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="100%"
-                            height="100%"
-                            viewBox="0 0 24 24"
-                            className="group-hover:fill-red-100 fill-red-500 stroke-0"
-                          >
-                            <path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z" />
-                          </svg>
-                        </button>
-                      </div>
-                    )}
-                    <TextareaAutosize
-                      required
-                      disabled={!isOwner}
-                      onChange={(e) => {
-                        let newArr = [...data];
-                        newArr[i] = { ...newArr[i], label: e.target.value };
-                        setData(newArr);
-                      }}
-                      className="text-sm bg-transparent w-full focus:bg-blue-500/50 focus:outline-0 p-2 rounded text-center"
-                      value={t.label}
-                    />
-                  </td>
-                  <td
-                    width="75%"
-                    className="text-sm border-blue-400 border-b leading-3"
-                  >
-                    <TextareaAutosize
-                      required
-                      disabled={!isOwner}
-                      onChange={(e) => {
-                        let newArr = [...data];
-                        newArr[i] = { ...newArr[i], value: e.target.value };
-                        setData(newArr);
-                      }}
-                      className="text-sm bg-transparent w-full focus:bg-blue-500/50 p-2 focus:outline-0 rounded"
-                      value={t.value}
-                    />
-                    {/* <textarea className="bg-transparent w-full focus:bg-blue-500/50 p-2 focus:outline-0">
-                    {t.value}
-                  </textarea> */}
-                  </td>
-                </tr>
-              ))}
-              {emptyList.map((e, i) => (
-                <tr className="relative" key={`empty${i}`}>
-                  <td
-                    width="25%"
-                    className="font-medium border-blue-400 border-r border-b leading-3"
-                  >
+
+        <table className="border border-blue-400 rounded-t-md border-separate border-spacing-0 border-b-0 w-full">
+          <tbody>
+            {data.map((t, i: number) => (
+              <tr key={i} className="relative">
+                <td
+                  width="25%"
+                  className="font-medium border-blue-400 border-r border-b leading-3"
+                >
+                  {isOwner && (
                     <div className="absolute left-[-1.5rem] h-full justify-center flex flex-col gap-2">
                       <button
                         type="button"
-                        onClick={() =>
-                          setEmptyList((prev) =>
+                        onClick={() => {
+                          setData((prev) =>
                             prev.filter((g, index) => index != i)
-                          )
-                        }
+                          );
+                          setTrash((prev) => [...prev, t.id]);
+                        }}
                         className="group w-4 h-4 p-[0.1rem]"
                       >
                         <svg
@@ -154,35 +93,93 @@ const Trait = () => {
                         </svg>
                       </button>
                     </div>
-                    <TextareaAutosize
-                      required
-                      onChange={(e) => {
-                        let newArr = [...emptyList];
-                        newArr[i] = { ...newArr[i], label: e.target.value };
-                        setEmptyList(newArr);
-                      }}
-                      className="text-sm bg-transparent w-full focus:bg-blue-500/50 focus:outline-0 p-2 rounded text-center"
-                    />
-                  </td>
-                  <td
-                    width="75%"
-                    className="border-blue-400 border-b leading-3"
-                  >
-                    <TextareaAutosize
-                      required
-                      onChange={(e) => {
-                        let newArr = [...emptyList];
-                        newArr[i] = { ...newArr[i], value: e.target.value };
-                        setEmptyList(newArr);
-                      }}
-                      className="text-sm bg-transparent w-full focus:bg-blue-500/50 p-2 focus:outline-0 rounded"
-                    />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
+                  )}
+                  <TextareaAutosize
+                    required
+                    disabled={!isOwner}
+                    onChange={(e) => {
+                      let newArr = [...data];
+                      newArr[i] = { ...newArr[i], label: e.target.value };
+                      setData(newArr);
+                    }}
+                    className="text-sm bg-transparent w-full focus:bg-blue-500/50 focus:outline-0 p-2 rounded text-center"
+                    value={t.label}
+                  />
+                </td>
+                <td
+                  width="75%"
+                  className="text-sm border-blue-400 border-b leading-3"
+                >
+                  <TextareaAutosize
+                    required
+                    disabled={!isOwner}
+                    onChange={(e) => {
+                      let newArr = [...data];
+                      newArr[i] = { ...newArr[i], value: e.target.value };
+                      setData(newArr);
+                    }}
+                    className="text-sm bg-transparent w-full focus:bg-blue-500/50 p-2 focus:outline-0 rounded"
+                    value={t.value}
+                  />
+                  {/* <textarea className="bg-transparent w-full focus:bg-blue-500/50 p-2 focus:outline-0">
+                    {t.value}
+                  </textarea> */}
+                </td>
+              </tr>
+            ))}
+            {emptyList.map((e, i) => (
+              <tr className="relative" key={`empty${i}`}>
+                <td
+                  width="25%"
+                  className="font-medium border-blue-400 border-r border-b leading-3"
+                >
+                  <div className="absolute left-[-1.5rem] h-full justify-center flex flex-col gap-2">
+                    <button
+                      type="button"
+                      onClick={() =>
+                        setEmptyList((prev) =>
+                          prev.filter((g, index) => index != i)
+                        )
+                      }
+                      className="group w-4 h-4 p-[0.1rem]"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="100%"
+                        height="100%"
+                        viewBox="0 0 24 24"
+                        className="group-hover:fill-red-100 fill-red-500 stroke-0"
+                      >
+                        <path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z" />
+                      </svg>
+                    </button>
+                  </div>
+                  <TextareaAutosize
+                    required
+                    onChange={(e) => {
+                      let newArr = [...emptyList];
+                      newArr[i] = { ...newArr[i], label: e.target.value };
+                      setEmptyList(newArr);
+                    }}
+                    className="text-sm bg-transparent w-full focus:bg-blue-500/50 focus:outline-0 p-2 rounded text-center"
+                  />
+                </td>
+                <td width="75%" className="border-blue-400 border-b leading-3">
+                  <TextareaAutosize
+                    required
+                    onChange={(e) => {
+                      let newArr = [...emptyList];
+                      newArr[i] = { ...newArr[i], value: e.target.value };
+                      setEmptyList(newArr);
+                    }}
+                    className="text-sm bg-transparent w-full focus:bg-blue-500/50 p-2 focus:outline-0 rounded"
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
         {isOwner && (
           <button
             onClick={makeNew}
